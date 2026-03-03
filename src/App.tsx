512e95d272465d4ce3fef92e4b5fc67691727060
limploc <udindermana9@gmail.com>
2026-03-03 11:19:50 +0800

    refactor: clean up App component and remove unused CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
