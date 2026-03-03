88226ade0c76948a12db3047f1873026e03ff517
limploc <udindermana9@gmail.com>
2026-03-03 11:19:41 +0800

    feat: update Home component structure and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import Hero from '../components/Hero/Hero';
+import About from '../components/About/About';
+import TechStack from '../components/TechStack/TechStack';
+import Projects from '../components/Projects/Projects';
+
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
-    </div>
-  )
+    <main>
+      <Hero />
+      <About />
+      <TechStack />
+      <Projects />
+    </main>
+  );
 }
 
 export default Home;
